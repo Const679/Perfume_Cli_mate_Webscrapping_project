e17c0a41933533884a9543207754b9722a2d02ac
sandwish98 <133352430+sandwish98@users.noreply.github.com>
2024-01-19 17:36:57 +0100

    Add files via upload

---
+++ ex.py
@@ -0,0 +1,52 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jan 16 17:01:18 2024
+
+@author: fouzari
+"""
+
+{
+   "Top": {
+    "keys": 1,
+    "name": "Coco Chanel",
+    "brand": {
+      "name": "Chanel",
+      "engagement": "Cette marque...."
+    },
+    "Ingredient": {
+      "name": "Benzoate",
+      "endocrinien": "oui",
+      "allergène": "ça n'est pas allergène",
+      "description": "C'est un produit ......", 
+      "name": "citron",
+      "endocrinien": "non",
+      "allergène": "ça n'est pas allergène",
+      "description": "C'est un produit ......", 
+    },
+    
+    "Ingredient": {
+      "name": "Benzoate",
+      "endocrinien": "oui",
+      "allergène": "ça n'est pas allergène",
+      "description": "C'est un produit ......", 
+      "name": "citron",
+      "endocrinien": "non",
+      "allergène": "ça n'est pas allergène",
+      "description": "C'est un produit ......", 
+    }
+  },
+  "Top": {
+    "keys": 2,
+    "name": "My Way",
+    "brand": {
+      "name": "Chanel",
+      "engagement": "Sans engagement"
+    },
+    "Ingredient": {
+      "name": "Water",
+      "endocrinien": "oui",
+      "allergène": "ça n'est pas allergène",
+      "description": "C'est un produit ......"
+    }
+  }
+}
